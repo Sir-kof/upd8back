749cdd433f671bd60c367b4f6e1aed0378baba06
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2022-11-04 19:06:53 -0300

    test: ensure DbGetUser call GetUserRepository with correct values

---
+++ src/data/usecases/get-user/db-get-user.spec.ts
@@ -0,0 +1,55 @@
+import { IUserModel, IGetUserModel, IGetUserRepository } from './db-get-user-protocolls'
+import { DbGetUser } from './db-get-user'
+
+const makeFakeUser = (): IUserModel => ({
+  id: 'valid_id',
+  cpf: 'valid_cpf',
+  name: 'valid_name',
+  address: 'valid_address',
+  birthday: '10/10/2022',
+  gender: 'male',
+  state: 'Rio de Janeiro',
+  cidade: 'Duque de Caxias'
+})
+
+const makeFakeUserData = (): IGetUserModel => ({
+  cpf: 'valid_cpf',
+  name: 'valid_name',
+  address: 'valid_address',
+  birthday: '10/10/2022',
+  gender: 'male',
+  state: 'Rio de Janeiro',
+  cidade: 'Duque de Caxias'
+})
+
+const makeGetUserRepository = (): IGetUserRepository => {
+  class GetUserRepositoryStub implements IGetUserRepository {
+    async get (User: IGetUserModel): Promise<IUserModel> {
+      return await new Promise(resolve => resolve(makeFakeUser()))
+    }
+  }
+  return new GetUserRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbGetUser
+  getUserRepositoryStub: IGetUserRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getUserRepositoryStub = makeGetUserRepository()
+  const sut = new DbGetUser(getUserRepositoryStub)
+  return {
+    sut,
+    getUserRepositoryStub
+  }
+}
+
+describe('DbGetUser Usecase', () => {
+  test('Should call GetUserRepository with correct values', async () => {
+    const { sut, getUserRepositoryStub } = makeSut()
+    const getSpy = jest.spyOn(getUserRepositoryStub, 'get')
+    await sut.get(makeFakeUserData())
+    expect(getSpy).toHaveBeenCalledWith(makeFakeUserData())
+  })
+})
