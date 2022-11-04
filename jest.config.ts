34ac85878eb111c841f4f83f8e88cb9462528d98
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2022-11-04 17:56:25 -0300

    refactor: create script test unit and test integration

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,9 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'jest-environment-node',
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
